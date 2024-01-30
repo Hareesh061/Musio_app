51cf11944f3b447b9cd956c0925eb3c855af65cf
Hareesh Naik <hareeshnaik836@gmail.com>
2024-01-30 11:27:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -45,8 +45,43 @@ function App() {
  <div className="container">
   <div className="row">
     <div className="col">
-      <button onClick={getTracks} className="btn btn-primary">get data</button>
+      <button onClick={getTracks} className="btn btn-primary ">get data</button>
     </div>
+   {
+    albums.map((element) => {
+      return (
+      <div key={element.id} className="col">
+        {/* <img
+        className="border-2 w-100"
+        src={element.images[0].url} 
+        alt="img"/>
+
+        <p>{element.id}</p> */}
+
+
+
+        <div className="card" style={{ width: "18rem" }}>
+  <img className="card-img-top" src={element.images[0].url} alt="Card image cap" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </p>
+    <a href="#" className="btn btn-primary">
+      Go somewhere
+    </a>
+  </div>
+</div>
+
+
+
+
+
+        </div>
+      );
+    })
+   }
 
   </div>
  </div>
