1ce6c402a936658b0d093ecd51aae7f9ed585bd3
Hareesh Naik <hareeshnaik836@gmail.com>
2024-02-02 13:24:00 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const   [keyword, setKeyword] = useState("");
   const [albums,setTracks] = useState([])
 
   const getTracks = async() =>{
-    let data = await fetch("https://v1.nocodeapi.com/harry019/spotify/cOHqTEjyYXbOrdMS/search?q=<q>");
+    let data = await fetch(`https://v1.nocodeapi.com/harry019/spotify/cOHqTEjyYXbOrdMS/search?q=${keyword}`);
     let convertedData = await data.json();
     console.log(convertedData.albums.items);
     setTracks(convertedData.albums.items);
   };
 
   return <>
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
+<nav className="navbar navbar-dark navbar-expand-lg navbar-light bg-dark">
   <a className="navbar-brand" href="#">
     Musio.co
   </a>
@@ -27,42 +28,40 @@ function App() {
   >
     <span className="navbar-toggler-icon" />
   </button>
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+  <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
     
       <input
+      value={keyword}
+      onChange={event=>setKeyword(event.target.value)}
         className="form-control mr-2 w-50"
         type="search"
-        placeholder="Search"
+        placeholder="Search your  music here..."
         aria-label="Search"
       />
-      <button className="btn btn-outline-success " type="submit">
-        Search
+      <button onClick={getTracks} className="btn btn-outline-success " >
+        Search 
       </button>
   
   </div>
 </nav>
-<div className="col">
-      <button onClick={getTracks} className="btn btn-primary ">get data</button>
-    </div>
+
  <div className="container">
   <div className="row">
+
    
    {
     albums.map((element) => {
       return (
-      <div key={element.id} className="col">
+      <div key={element.id} className="col-lg-3 col-md-6 py-2">
 
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
   <img className="card-img-top" src={element.images[0].url} alt="Card image cap" />
   <div className="card-body">
     <h5 className="card-title">{element.name}</h5>
     <p className="card-text">
       Artist:  {element.artists[0].name}
     </p>
-    <audio src={element.external_urls }  controls className="w-100"></audio>
-    <a href="#" className="btn btn-primary">
-      Go somewhere
-    </a>
+    <audio src={element.preview_url }  controls className="w-100"></audio>
   </div>
 </div>
 
